868b74d9be05ee11a265266b543cb8c19a1911a9
Unknown <antonio.omt@gmail.com>
2019-06-04 01:47:03 +0300

    character controller

--- StraySheep/Assets/Code/Player/Player.cs
+++ StraySheep/Assets/Code/Player/Player.cs
@@ -2,28 +2,62 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
-    CharacterController charControl;
+    CharacterController controller;
+    Animator anim;
 
-    [SerializeField] private float gravity;
-    [SerializeField] private float speed;
+    [SerializeField] private float moveSpeed = 6;
+    [SerializeField] private float minJumpHeight = 1;
+    [SerializeField] private float maxJumpHeight = 4;
+    [SerializeField] private float timeToJumpPeak = .4f;
+    [SerializeField] private float accelerationTimeInAir = .2f;
+    [SerializeField] private float accelerationTimeGrounded = .1f;
+
+    private float gravity;
+    private float minJumpVelocity;
+    private float maxJumpVelocity;
+    private Vector3 velocity;
+    float movementSmoothing;
 
     // Start is called before the first frame update
     void Start()
     {
-        charControl = GetComponent<CharacterController>();
+        controller = GetComponent<CharacterController>();
+
+        //calculate gravity and velocity from wanted jump height and time to peak
+        gravity = -(2 * maxJumpHeight / Mathf.Pow(timeToJumpPeak, 2));
+        maxJumpVelocity = Mathf.Abs(gravity) * timeToJumpPeak;
+        minJumpVelocity = Mathf.Sqrt(2 * Mathf.Abs(gravity) * minJumpHeight);
+        print("Gravity: " + gravity + " Jump Velocity: " + maxJumpVelocity);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (controller.collisions.above || controller.collisions.below)
+        {
+            velocity.y = 0;
+        }
+
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (Input.GetKeyDown(KeyCode.Space)|| Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (controller.collisions.below)
+            velocity.y = maxJumpVelocity;
+        }
+        if (Input.GetKeyUp(KeyCode.Space) || Input.GetKeyUp(KeyCode.UpArrow))
+        {
+            if (velocity.y > minJumpVelocity)
+            velocity.y = minJumpVelocity;
+        }
+
+        float targetVelocityX = input.x * moveSpeed;
+        velocity.x = Mathf.SmoothDamp(velocity.x, targetVelocityX, ref movementSmoothing, (controller.collisions.below)?accelerationTimeGrounded:accelerationTimeInAir);
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 
-    public void PlayerMovementUpdate()
-    {
-        charControl.ApplyGravity(gravity);
-        charControl.ScrollForward(speed);
-    }
 }
