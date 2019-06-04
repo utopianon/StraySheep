156f3dfe8368e74f6ef1f5329833b04ce4787a05
Toni Sundell <toni.sundell@cs.tamk.fi>
2019-06-04 11:57:46 +0300

    Made pickup and danger WIP

---
+++ StraySheep/Assets/Code/Stuff/Pickup.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pickup : MonoBehaviour
+{
+    public int score;
+
+    public void Die()
+    {
+        Destroy(gameObject);
+    }
+}
